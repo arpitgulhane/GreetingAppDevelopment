d400d777d06fdd23751749185b747cbff24b780f
DESKTOP-VFH6PON\USER <arpit@edupluscampus.com>
2023-06-14 08:05:53 +0530

    return JSON HTTP Methods

--- src/main/java/com/greetingDevelopment/greeting/controller/Greetingcontroller.java
+++ src/main/java/com/greetingDevelopment/greeting/controller/Greetingcontroller.java
@@ -10,6 +10,12 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class Greetingcontroller {
 
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
 
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(name = "name",defaultValue = "arpit") String name){
+        return new Greeting(counter.incrementAndGet(),String.format(template,name));
+    }
 
 }
